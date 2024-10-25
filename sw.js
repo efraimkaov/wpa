8c5863914276f7aafc0091a71c8be2e51f33983d
efraimkaov <63643635+efraimkaov@users.noreply.github.com>
2024-10-25 21:04:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,66 +1,22 @@
-const staticCacheName = 'site-static-v2'
-const dynamicCacheName = 'site-dynamic-v1'
-const assets = [
-    '/wpa/',
-    '/wpa/index.html',
-    '/wpa/styles.css',
-    '/wpa/a.wav',
-    '/wpa/b.wav'
+
+// Change this to your repository name
+var GHPATH = '/github-page-pwa';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'gppwa_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/styles.css`,
+  `${GHPATH}/a.wav`,
+  `${GHPATH}/b.wav`
 ]
-
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if (keys.length > size) {
-        cache.delete(keys[0]).then(limitCacheSize(name, size))
-      }
-    })
-  })
-}
-
-// install event
-self.addEventListener('install', evt => {
-  //console.log('service worker installed')
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets')
-      cache.addAll(assets)
-    })
-  )
-})
-
-// activate event
-self.addEventListener('activate', evt => {
-  //console.log('service worker activated')
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      //console.log(keys)
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      )
-    })
-  )
-})
-
-// fetch event
-self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt)
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone())
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 15)
-          return fetchRes
-        })
-      })
-    }).catch(() => {
-      if (evt.request.url.indexOf('.html') > -1) {
-        return caches.match('/twenty48/static/tmpl/404.html')
-      }
-    })
-  )
-})
