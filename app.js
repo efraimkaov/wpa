40fbb35c293d60976562b9660fabf34b71bb9ffe
efraimkaov <63643635+efraimkaov@users.noreply.github.com>
2024-10-26 21:21:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker
-  .register("/wpa/service-worker.js", { scope: "/wpa/" })
+  .register("service-worker.js")
   .then(() => console.log("Service Worker registered"))
   .catch((error) => console.log("Service Worker registration failed:", error));
 }
