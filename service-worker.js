2940d288d8216f54cad10e0715bcabd1a906b2ee
efraimkaov <63643635+efraimkaov@users.noreply.github.com>
2024-10-26 05:46:24 +0300

    Update and rename app.js to service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker
-  .register("/wpa/sw.js", { scope: "/wpa/" })
+  .register("/wpa/service-worker.js", { scope: "/wpa/" })
   .then(() => console.log("Service Worker registered"))
   .catch((error) => console.log("Service Worker registration failed:", error));
 }
